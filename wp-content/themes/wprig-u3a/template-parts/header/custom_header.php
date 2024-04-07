68c1712289c8c96b37b739e2e85ea939cf9e1329
Amy_yanqing <yanqing.zhong@my.jcu.edu.au.com>
2024-04-07 17:32:28 +1000

    Your commit message here

--- wp-content/themes/wprig-u3a/template-parts/header/custom_header.php
+++ wp-content/themes/wprig-u3a/template-parts/header/custom_header.php
@@ -14,4 +14,8 @@ if ( ! has_header_image() ) {
 ?>
 <figure class="header-image">
 	<?php the_header_image_tag(); ?>
+	<figcaption class="header-text"><?php esc_html_e( '(◠‿◠) This is not your local U3A website;
+	rather, it is the website of U3A Online,
+	a virtual U3A Group where everything is conducted online.', 'wp-rig' ); ?>
+</figcaption>
 </figure><!-- .header-image -->
